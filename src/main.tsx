66c6ad288cd7dc6bc7346001777439be5be242aa
HM1 <mrtoscano94@gmail.com>
2023-08-05 15:32:21 -0600

    Some changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+import Topbar from './components/Topbar.component.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    {/* <Topbar/> */}
+    <RouterProvider router={router} ></RouterProvider>
   </React.StrictMode>,
 )
